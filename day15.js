6ece20cc6b3336d1fc2050f3f90239dc45290374
James Ng <njames15@gmail.com>
2022-12-15 08:12:09 -0500

    day 15 solution

---
+++ day15.js
@@ -0,0 +1,24 @@
+/* 
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palidrome, return true. If it isn't, return false. 
+*/
+
+function isPalindrome(str){
+  let i = 0, j = str.length - 1;
+  while (i < j) {
+      if (str[i] !== str[j]) return false;
+      i++;
+      j--;
+  }
+  return true;
+}
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
