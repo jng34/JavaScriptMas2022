a17ba2f02be84ee452313840c15e039242ad5435
JamesNg <njames15@gmail.com>
2022-12-19 13:01:44 -0500

    day 19 solution

---
+++ day19.js
@@ -0,0 +1,36 @@
+function centuryFromYear(num) {
+  return num % 100 == 0 ? num / 100 : Math.floor(num / 100) + 1;
+}
+
+/**
+ * Test Suite
+ */
+describe("centuryFromYear()", () => {
+  it("returns current century", () => {
+    // arrange
+    const year = 1900;
+
+    // act
+    const result = centuryFromYear(year);
+
+    // log
+    console.log("result 1: ", result);
+
+    // assert
+    expect(result).toBe(20);
+  });
+
+  it("returns current century for edge case of start of century", () => {
+    // arrange
+    const year = 1700;
+
+    // act
+    const result = centuryFromYear(year);
+
+    // log
+    console.log("result 2: ", result);
+
+    // assert
+    expect(result).toBe(17);
+  });
+});
